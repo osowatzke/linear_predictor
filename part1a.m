6ad12e7d81d850a8198bbc18dcb15ad06dfacf8d
Owen Sowatzke <osowatzke@gmail.com>
2021-03-06 20:30:16 -0500

    Added computations to part 1a.

--- part1a.m
+++ part1a.m
@@ -15,4 +15,18 @@ function part1a(djiaw)
     xlabel('Week');
     ylabel('Dow Jones Industrial Average');
     title('Plot of the Dow Jones Average on a Semi-Logarithmic Scale');
+    
+    % if you had put all your money in the stock market, determine how 
+    % much you would have at the end of the investment interval
+    value = djiaw(end)/djiaw(1)*1000;
+    fprintf('If you invested $1000 in the stock market, you would have\n');
+    fprintf('$%.2f at the end of investment interval (%d weeks)\n', ...
+        value, num_weeks-1);
+    
+    % if you had put all your money in the bank, determine how many weeks
+    % it would take to earn as much as the DJIA?
+    r = 0.03;
+    N = log(value/1000)/log(1+r/52);
+    fprintf('\nIf you invested $1000 in the bank at %%3 APR, it would take\n');
+    fprintf('%d weeks to earn as much as the DJIA\n', round(N))
 end
