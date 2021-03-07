1d64e01754fab1f944e9e32ca9830f33cba0baea
Owen Sowatzke <osowatzke@gmail.com>
2021-03-06 20:43:14 -0500

    Code for linear predictor coefficients.

---
+++ part1b.m
@@ -0,0 +1,25 @@
+% output linear predictor coefficients
+% for p=3 and N=520
+function a = part1b(djiaw)
+
+    % problem specs
+    p = 3;
+    N = 520;
+    
+    % define empty matrix X
+    X = zeros(N-p,p);
+    
+    % initialize matrix X
+    for n = 1:N-p
+        for k=0:p-1
+            X(n,k+1) = djiaw(n+k);
+        end
+    end
+    
+    % calculate vector x
+    x = djiaw(p+1:N);
+    
+    % determine linear predictor coefficients
+    % emitting semicolon, outputs results to command window
+    a = -X\x
+end
