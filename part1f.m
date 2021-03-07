2bdffdfa8b5687b8ab9e403a0271ac6a311b8f3e
Owen Sowatzke <osowatzke@gmail.com>
2021-03-07 04:01:48 -0500

    Added code for linear prediction of last decade.

--- part1f.m
+++ part1f.m
@@ -41,4 +41,54 @@ function part1f(djiaw)
     % output lower bound
     fprintf('\n\tLower bound on how much you could make if you only\n');
     fprintf('\tinvested your money in the stock market: $%.2f\n', investment);
+    
+    % number of linear predictor coefficients
+    p = 10;
+    
+    % determine matrix X
+    X = zeros(N-p,p);
+    for n = 1:N-p
+        for k = 0:p-1
+            X(n,k+1) = djiaw(n+k);
+        end
+    end
+    
+    % determine vector x
+    x = djiaw(p+1:N);
+    
+    % determine linear predictor coefficients
+    % identical to coefficients in part 1e
+    a = -X\x;
+    
+    % determine predicted values
+    % note the omission of p-1 coefficients
+    xhat = filter(-flip(a),1,djiaw(end-N-p+1:end-1));
+    xhat = xhat(p:end);
+    
+    plot(1:N,xhat,1:N,djiaw(end-N+1:end))
+    
+    % initial investment
+    investment = 1000;
+    
+    % loop makes 520 trading decisions
+    for n=1:N
+        
+        % predicted gain using linear predictor
+        predicted_gain = xhat(n)/djiaw(n+end-N-1);
+        
+        % stock market gain (bank gain was already calculated)
+        DJIA_gain = djiaw(n+end-N)/djiaw(n+end-N-1);
+        
+        % determine whether to invest in bank or stock market
+        if (predicted_gain > bank_gain)
+            investment = DJIA_gain * investment;
+        else
+            investment = bank_gain * investment;
+        end
+    end
+    
+    % output results of linear predictor
+    fprintf('\n\tHow much you would make if you based your\n');
+    fprintf('\tdecisions on the linear predictor: $%.2f\n', investment);
+    
 end
