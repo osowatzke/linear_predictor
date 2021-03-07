5865f6ab7b7038a33653bd59a461810f3947f64c
Owen Sowatzke <osowatzke@gmail.com>
2021-03-07 00:55:02 -0500

    Added lines of code to distinguish between first and last decade results.

--- part1a.m
+++ part1a.m
@@ -32,4 +32,10 @@ function part1a(djiaw)
     N = log(value/1000)/log(1+r/52);
     fprintf('\nIf you invested $1000 in the bank at %%3 APR, it would take\n');
     fprintf('%d weeks to earn as much as the DJIA\n', round(N))
+    
+    % if you had put all your money in the bank, determine what APR would 
+    % you need to make the same amount
+    r = 52*((value/1000)^(1/(num_weeks-1))-1);
+    fprintf('\nIf you invested $1000 in the bank for %d weeks,\n', num_weeks-1);
+    fprintf('you would need an APR of %%%.2f to earn as much as the DJIA\n', 100*r);
 end
